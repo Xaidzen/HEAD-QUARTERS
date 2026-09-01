52b6fa49a24c80727595d601dc66f8ffd5e51c55
Xaidzen <uzumakinaruto032812@gmail.com>
2026-09-01 10:12:29 +0900

    Remove configuration file src/utils/config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,15 +1,1 @@
-module.exports = {
-  guildId: process.env.TARGET_GUILD_ID,
 
-  roles: {
-    unverified: process.env.UNVERIFIED_ROLE_ID,
-    verified: process.env.VERIFIED_ROLE_ID
-  },
-
-  channels: {
-    enter: process.env.ENTER_CHANNEL_ID,
-    service: process.env.SERVICE_CHANNEL_ID
-  },
-
-  tornApiUrl: 'https://api.torn.com'
-};
