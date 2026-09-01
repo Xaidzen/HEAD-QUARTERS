1cf84bfba022a111f85dabee34b7d7eba7511735
Xaidzen <uzumakinaruto032812@gmail.com>
2026-09-01 10:13:30 +0900

    Remove messageCreate event handler

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,11 +1,1 @@
-module.exports = {
-  name: 'messageCreate',
 
-  async execute(message) {
-    if (message.author.bot) return;
-
-    console.log(
-      `📩 ${message.author.tag}: ${message.content}`
-    );
-  }
-};
