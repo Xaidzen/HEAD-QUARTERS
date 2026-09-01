bdc183a2745a7e83b1fe6ee137311f9490db5011
Xaidzen <uzumakinaruto032812@gmail.com>
2026-09-01 10:13:13 +0900

    Remove clientReady event handler

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,1 @@
-module.exports = {
-  name: 'clientReady',
-  once: true,
 
-  execute(client) {
-    console.log(`✅ Logged in as ${client.user.tag}`);
-    console.log(`🤖 Bot ID: ${client.user.id}`);
-  }
-};
