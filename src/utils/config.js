f7776cd4424304853140fffdb12997458214d36a
Xaidzen <uzumakinaruto032812@gmail.com>
2026-09-01 10:20:40 +0900

    Add configuration for Discord bot using environment variables

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,1 +1,20 @@
+require('dotenv').config();
 
+module.exports = {
+  discordToken: process.env.DISCORD_TOKEN,
+  clientId: process.env.CLIENT_ID,
+
+  guildId: process.env.TARGET_GUILD_ID,
+
+  roles: {
+    verified: process.env.VERIFIED_ROLE_ID,
+    unverified: process.env.UNVERIFIED_ROLE_ID
+  },
+
+  channels: {
+    enter: process.env.ENTER_CHANNEL_ID,
+    service: process.env.SERVICE_CHANNEL_ID
+  },
+
+  tornApiUrl: process.env.TORN_API_URL || 'https://api.torn.com'
+};
